5ac9ea3b081f9148e056e5e787ee7879aba155df
Ghost <priyammittal197@gmail.com>
2025-06-26 16:24:21 +0530

    update

--- src/http/api.ts
+++ src/http/api.ts
@@ -20,22 +20,35 @@ export interface FormsPopProps {
   class: string;
   message: string;
 }
+// const api = axios.create({
+//     baseURL: import.meta.env.VITE_BASE_URL,
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// })
 const api = axios.create({
-    // baseURL: "https://api.web3forms.com/submit/",
-    baseURL: import.meta.env.VITE_PUBLIC_BASE_URL,
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
+  baseURL: "https://api.web3forms.com/submit",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
+// const popForm = async (data: FormsPopProps) => {
+//     const res = await api.post('', data)
+//     return res
+// }
 const popForm = async (data: FormsPopProps) => {
-    const res = await api.post('', data)
-    return res
-}
+  const res = await api.post("", data);
+  return res.data;
+};
+// const submitForm = async (data: FormProps) => {
+//     const res = await api.post('', data)
+//     return res
+// }
 const submitForm = async (data: FormProps) => {
-    const res = await api.post('', data)
-    return res
-}
+  const res = await api.post("", data);
+  return res.data;
+};
 
 export {
     popForm,
